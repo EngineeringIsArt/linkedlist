5e4bc1ec96f8e19c344d6649b2d1b3fa63656393
Jaysun Lee <lee.jaysun@gmail.com>
2019-10-23 21:00:17 +1100

    Write rudimentary LinkedList implementation.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace linkedlist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var head = new Node(1);
+            head.Next = new Node(2);
+            head.Next.Next = new Node(3);
+
+            Node.PrintList(head);
+        }
+    }
+
+    class Node
+    {
+        public Node(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        public static void PrintList(Node node)
+        {
+            while (node != null)
+            {
+                Console.Write($"{node.Value} -> ");
+                node = node.Next;
+            }
         }
     }
 }
